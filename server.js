39d2cea5ac9b9a6d8763be413ad3c523af7bee0d
adam_m_ridwan <111061864+amreditor@users.noreply.github.com>
2023-12-19 15:52:20 +0700

    menambahkan statusCode
    menambahkan deskripsi status kode jika tidak bisa di akses nya halaman

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
  
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+    // response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
     response.statusCode = 200;
 
     const { method,url } = request;
@@ -10,7 +12,22 @@ const requestListener = (request, response) => {
         if(method === 'GET') {
             // curl -X GET http://localhost:5000
             response.end('<h1>Ini adalah homepage</h1>');
+        } else if(method === 'POST'){
+            // curl -X POST http://localhost:5000
+            // curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"Dicoding\"}"
+            let badan = [];
+
+            request.on('data', (hunk) => {
+                badan.push(hunk);
+            });
+
+            request.on('end', () => {
+                badan = Buffer.concat(badan).toString();
+                const { nama } = JSON.parse(body);
+                response.end(`<h1>Halo, ${nama}! ini adalah halaman utama`);
+            })
         } else {
+            response.statusCode = 404;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if(url === '/about') {
@@ -31,9 +48,11 @@ const requestListener = (request, response) => {
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 404;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         // curl -X <any> http://localhost:5000
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
