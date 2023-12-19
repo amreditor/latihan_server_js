e63b92d1567841c6ac6c69d5a6a3550ea5f134a7
adam_m_ridwan <111061864+amreditor@users.noreply.github.com>
2023-12-19 17:33:44 +0700

    mengubah resnponse dengan objek
    mengubah response dengan objek dan di ubahdengan stringify dan menampilkan message

--- server.js
+++ server.js
@@ -9,33 +9,29 @@ const requestListener = (request, response) => {
     const { method,url } = request;
 
     if(url === '/') {
+
         if(method === 'GET') {
             // curl -X GET http://localhost:5000
-            response.end('<h1>Ini adalah homepage</h1>');
-        } else if(method === 'POST'){
-            // curl -X POST http://localhost:5000
-            // curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"Dicoding\"}"
-            let badan = [];
-
-            request.on('data', (hunk) => {
-                badan.push(hunk);
-            });
-
-            request.on('end', () => {
-                badan = Buffer.concat(badan).toString();
-                const { nama } = JSON.parse(body);
-                response.end(`<h1>Halo, ${nama}! ini adalah halaman utama`);
-            })
+            response.end(JSON.stringify({
+                message: 'ini adalah homepage'
+            }));
         } else {
             response.statusCode = 404;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
+
     } else if(url === '/about') {
+
         if(method === 'GET') {
             // curl -X GET http://localhost:5000/about
-            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+            response.end(JSON.stringify({
+                message: 'ini adalah halaman about'
+            }))
         } else if(method === 'POST') {
             // curl -X POST http://localhost:5000/about
+            // curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"Dicoding\"}"
             let body = [];
     
             request.on('data', (chunk) => {
@@ -49,12 +45,17 @@ const requestListener = (request, response) => {
             });
         } else {
             response.statusCode = 404;
-            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `halaman ini tidak di temukan dengan ${method} request`,
+            }));
         }
+        
     } else {
         response.statusCode = 404;
         // curl -X <any> http://localhost:5000
-        response.end('<h1>Halaman tidak ditemukan!</h1>');
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan',
+        }));
     }
     
 
